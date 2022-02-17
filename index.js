459e2caebaad277fdeda06f9580ff833515bd6bc
Alberto Vidal <alberto_vidalm@protonmail.com>
2022-02-17 21:29:29 +0100

    :sparkles: created model for thing

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 require("dotenv").config();
 const serverSays = require("debug")("things:root:");
+const connectDB = require("./db");
 const raiseServer = require("./server");
 
 const port = process.env.SERVER_PORT;
+const dbkey = process.env.MONGO_LOGIN;
 
 (async () => {
   try {
+    await connectDB(dbkey);
     await raiseServer(port);
   } catch (error) {
     serverSays(error);
