d2f53718dad2c3e8addac130b53821cd6f4a5681
Alberto Vidal <alberto_vidalm@protonmail.com>
2022-02-17 19:00:33 +0100

    :sparkles: managed error on server

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const serverSays = require("debug")("server:");
+const chalk = require("chalk");
 const express = require("express");
 
 const app = express();
@@ -8,3 +9,13 @@ const port = process.env.SERVER_PORT;
 const server = app.listen(port, () =>
   serverSays(`server listening at ${port}`)
 );
+
+server.on("error", (error) => {
+  serverSays(
+    chalk.red(
+      `Error: ${
+        error.code === "EADDRINUSE" ? `port ${port} busy` : error.message
+      }`
+    )
+  );
+});
